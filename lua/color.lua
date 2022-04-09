c6ba2fb594c738795ca4f739a642306da6e97e58
asthasr <466533+asthasr@users.noreply.github.com>
2022-04-09 11:27:04 -0400

    Clean up color lua

--- lua/color.lua
+++ lua/color.lua
@@ -1,12 +1,10 @@
-vim.cmd('colorscheme nord')
--- vim.o.background = 'light'
---
---
-vim.cmd('highlight Comment gui=italic')
+vim.cmd'colorscheme nord'
 
-vim.cmd('highlight clear String')
-vim.cmd('highlight String gui=italic')
+vim.cmd'highlight Comment gui=italic'
 
-vim.cmd('highlight Normal ctermbg=None guibg=None')
-vim.cmd('highlight SignColumn ctermbg=NONE guibg=NONE')
-vim.cmd('highlight LineNr ctermbg=NONE guibg=NONE')
+vim.cmd'highlight clear String'
+vim.cmd'highlight String gui=italic'
+
+vim.cmd'highlight Normal ctermbg=None guibg=None'
+vim.cmd'highlight SignColumn ctermbg=NONE guibg=NONE'
+vim.cmd'highlight LineNr ctermbg=NONE guibg=NONE'
